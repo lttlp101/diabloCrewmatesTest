230e24b579b684ed839cb42c8fb2c57f7745a2bf
Patrick <64219075+lttlp101@users.noreply.github.com>
2025-04-05 16:20:44 -0700

    Updated project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import diabloGroupPic from "../../assets/branding/diablo_all_classes.png";
 import diabloTitleLogo from "../../assets/branding/diablo_logo.webp";
-
 import styles from "./Home.module.css";
 
 const Home = () => {
