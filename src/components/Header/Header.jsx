9b69006464f8d218c506dd791641557d19efff7d
Patrick <64219075+lttlp101@users.noreply.github.com>
2025-04-04 20:37:20 -0700

    Updated whole project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,25 @@
+// components/Header/Header.jsx
+
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className={styles.header}>
+			<Link to="/" className={styles.headerLink}>
+				{/* Logo and title are wrapped in a Link component */}
+				<div className={styles.logoContainer}>
+					<img
+						src="/src/assets/branding/diablo-crewmates-logo.png"
+						alt="Diablo Crewmates Logo"
+						className={styles.logo}
+					/>
+				</div>
+				<h1 className={styles.title}>Diablo Crewmates</h1>
+			</Link>
+		</div>
+	);
 };
 
 export default Header;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,16 +2,15 @@
 
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styles from "./Navbar.module.css";
 import Header from "../Header/Header";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
+			<Header />
+
 			<ul className={styles.navList}>
-				<li className={styles.navItem}>
-					<Header />
-				</li>
 				<li className={styles.navItem}>
 					<NavLink
 						to="/"
